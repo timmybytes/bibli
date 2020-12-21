1bf6674809873670544f8e3956d068eb9d3aafc8
Timothy Merritt <timmybytes@gmail.com>
2020-12-21 07:10:48 -0800

    (feat): Add alternative API --WIP

--- src/AddBook.jsx
+++ src/AddBook.jsx
@@ -10,6 +10,51 @@ const AddBook = () => {
     setName(e.target.value);
   };
 
+  // const fetchOpenBook = async name => {
+  //   try {
+  //     const response = await fetch(`http://openlibrary.org/search.json?q=${name}`, {
+  //       method: 'GET',
+  //       credentials: 'same-origin',
+  //     });
+  //     const { openTitle } = await response.json();
+  //     console.log(openTitle);
+  //   } catch {
+  //     (error) {
+  //       console.error(error)
+  //     }
+  //   } finally {
+  //     console.log('object');
+  //   }
+  // };
+
+  const getOLBook = async name => {
+    try {
+      const response = await fetch(
+        `http://openlibrary.org/search.json?q=${name}&jscmd=data&format=json`,
+        {
+          method: 'GET',
+          credentials: 'same-origin',
+        }
+      );
+      var resBooks = await response.json();
+      resBooks = resBooks.docs;
+      console.log(resBooks);
+      const [
+        {
+          title: resTitle,
+          author_name: resAuthor,
+          description: resDescription,
+          first_publish_year: resYear,
+          cover_i: resCover,
+        },
+      ] = resBooks;
+      const resCoverArt = `https://covers.openlibrary.org/b/id/${resCover}.jpg",`;
+      console.log(resTitle, resAuthor[0], resYear, resDescription, resCoverArt);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const fetchBook = async name => {
     try {
       const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=title:${name}`, {
@@ -38,14 +83,17 @@ const AddBook = () => {
   const addBook = e => {
     e.preventDefault();
     fetchBook(name);
+    getOLBook(name);
   };
 
   return (
-    <form className='add-book__form' onSubmit={addBook}>
-      <label htmlFor='name'>Enter book name</label>
-      <input type='text' name='name' value={name} onChange={updateName} />
-      <button>Submit</button>
-    </form>
+    <>
+      <form className='add-book__form' onSubmit={addBook}>
+        <label htmlFor='name'>Enter book name</label>
+        <input type='text' name='name' value={name} onChange={updateName} />
+        <button>Submit</button>
+      </form>
+    </>
   );
 };
 
